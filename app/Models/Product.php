59864fa7ecfc3d09ae9b884f071a88f492845afc
MAZEN Kenjrawi <mazen270@hotmail.com>
2018-02-07 23:52:05 +0200

    Sale Price & Stock indicators support

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,10 @@ class Product extends Model
 {
     const LOW_STOCK_IDENTIFIER = 5;
 
+    const DEFAULT_CURRENCY = '$';
+
+    const CURRENCY_TO_LEFT = true;
+
     public function outOfStock()
     {
         return $this->stock === 0;
@@ -22,4 +26,22 @@ class Product extends Model
         return (bool) ($this->stock >= $qty);
     }
 
+    public function getPrice()
+    {
+        if(($this->sale_price != 0) && ($this->price > $this->sale_price))
+        {
+            return $this->sale_price;
+        }
+        return $this->price;
+    }
+
+    public function getPriceWithCurrency()
+    {
+        if(self::CURRENCY_TO_LEFT)
+        {
+            return self::DEFAULT_CURRENCY.' '. $this->getPrice(); 
+        } else {
+        return $this->getPrice() .' '. self::DEFAULT_CURRENCY;             
+        }
+    }
 }

dc6a9fdd5d754c5661c55b098503b5de1715902d
MAZEN Kenjrawi <mazen270@hotmail.com>
2018-02-14 00:01:57 +0200

    Printing the total excuted time

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,14 @@
 <?php
+# Starting Time
+$start = microtime(true);
+
 # Require The App.php File
 require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->run();
+
+# End Time
+$end = microtime(true);
+
+# Output in comment tag, the total excuted time
+echo "\n<!-- ".($end - $start)." -->";
